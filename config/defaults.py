3f782a1228992f36d94dcdf2329a741359c3955f
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-11-05 19:00:07 +0800

    Update defaults.py

--- config/defaults.py
+++ config/defaults.py
@@ -26,6 +26,7 @@ _C.DATASET.segm_downsampling_rate = 8
 # randomly horizontally flip images when train/test
 _C.DATASET.random_flip = True
 _C.DATASET.cropsize = (520,520)
+_C.DATASET.weight_init_scale = 1.0
 # -----------------------------------------------------------------------------
 # Model
 # -----------------------------------------------------------------------------
